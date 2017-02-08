b19f95391f0abd9a3bbd0676e9449b6ce16f2adc
zivInfo <zivuikit@gmail.com>
2017-02-08 11:30:46 +0800

    UDP 服务端

---
+++ UDPServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UDPServer
+//
+//  Created by zivInfo on 16/11/23.
+//  Copyright © 2016年 xiwangtech.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UDPServer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UDPServer
+//
+//  Created by zivInfo on 16/11/23.
+//  Copyright © 2016年 xiwangtech.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
